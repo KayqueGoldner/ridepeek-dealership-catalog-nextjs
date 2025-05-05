0f8a4ece1802e7635350fda79d058f6e192ee6e6
Kayque Goldner <kayquegoldner@gmail.com>
2025-05-05 01:29:19 -0300

    add pagination to cars list

--- src/app/(app)/cars/page.tsx
+++ src/app/(app)/cars/page.tsx
@@ -3,12 +3,17 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
 import { CarsView } from "@/modules/cars/ui/views/cars-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
 const CarsPage = () => {
   const queryClient = getQueryClient();
 
   void queryClient.prefetchQuery(trpc.cars.getManufacturers.queryOptions());
-  void queryClient.prefetchQuery(trpc.cars.getMany.queryOptions());
+  void queryClient.prefetchInfiniteQuery(
+    trpc.cars.getMany.infiniteQueryOptions({
+      limit: DEFAULT_LIMIT,
+    }),
+  );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
